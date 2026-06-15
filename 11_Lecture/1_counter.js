9374e74b4ede7743340f0fd31d951701ee0d6cb9
Aham08 <ahamgolash9340@gmail.com>
2026-06-15 11:38:24 +0530

    notes

---
+++ 11_Lecture/1_counter.js
@@ -0,0 +1,29 @@
+function createbankaccount(ib){
+    let b = ib;
+    function deposit(a){
+        b+= a;
+        console.log(b);
+    }
+    function withdraw(a){
+        b -= a;
+        console.log(b);
+    }
+    function checkbalance(){
+        console.log(b);
+    }
+    return{ deposit , withdraw , checkbalance};
+}
+
+const acc = createbankaccount(2000);
+acc.deposit(500);
+acc.withdraw(300);
+acc.checkbalance();
+console.log(acc.b);
+
+function multiplier(factor){
+    return function(numbers){
+        return factor*numbers;
+    }
+}
+
+const t = multiplier(3);
